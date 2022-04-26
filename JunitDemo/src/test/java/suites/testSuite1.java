2707ff9af4b2114f479dd6cae75297c54fb0e6c4
Razvan <razvan@Razvans-MacBook-Pro.local>
2022-04-26 12:33:22 +0300

    Added profile separation for suites

--- JunitDemo/src/test/java/suites/testSuite1.java
+++ JunitDemo/src/test/java/suites/testSuite1.java
@@ -1,14 +1,10 @@
 package suites;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.*;
 
-import java.lang.annotation.Annotation;
-
-@SelectPackages("tests")
-@IncludeTags({"test1","test2"})
 @Suite
 @SuiteDisplayName("A demo Test Suite running tests 1 and 2")
-@Tag("suite1")
+@IncludeTags(value = {"test1","test2"})
+@SelectPackages("tests")
 public class testSuite1{
 }
