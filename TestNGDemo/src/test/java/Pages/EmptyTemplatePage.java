1a0ffb6ffda90a0633d05fcdee66f7ed79a2cf16
Casian Stupari <casian.stupari@ebs.msft>
2022-04-27 14:58:00 +0300

    add testng demo

---
+++ TestNGDemo/src/test/java/Pages/EmptyTemplatePage.java
@@ -0,0 +1,18 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static utils.DriverBuilder.getDriver;
+
+public class EmptyTemplatePage {
+    public static WebElement agreeCookiePolicyBtn() {return getDriver.findElement(By.xpath("//*[contains(text(),'I agree')]"));}
+
+    public static WebElement searchField() {
+        return getDriver.findElement(By.cssSelector("input[title='Search']"));
+    }
+
+    public static java.util.List<WebElement> results (){
+        return getDriver.findElements(By.xpath("//div //a/h3"));
+    }
+}
