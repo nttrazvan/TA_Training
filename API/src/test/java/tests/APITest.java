a4a20dd482751fa6aed7ce87e23899571a8f8cfb
Liana Chis <ioana-liana.chis@nttdata.com>
2022-05-18 16:49:27 +0300

    API Homework

--- API/src/test/java/tests/APITest.java
+++ API/src/test/java/tests/APITest.java
@@ -1,29 +1,44 @@
 package tests;
 
+import com.github.fge.jsonschema.SchemaVersion;
+import com.github.fge.jsonschema.cfg.ValidationConfiguration;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.TestUtils;
-
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static com.github.fge.jsonschema.SchemaVersion.DRAFTV4;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class APITest {
     String baseURL = "https://petstore.swagger.io/v2";
     String urlSuffixAddPet = "/pet";
     String urlSuffixCreateUser = "/user";
     String urlSuffixLogIn = "/login";
-    String urlSuffixUpdateUser = "/user/test";
     File jsonUserData = new File("src/test/resources/user.json");
     File jsonUpdateUser = new File("src/test/resources/updateUser.json");
+    File jsonAddPet = new File("src/test/resources/addPet.json");
     TestUtils testUtils = new TestUtils();
     Response response;
     String user = "test";
 
-    @Test
+    @Test(priority = 2)
     public void addNewPet(){
         response = given()
                 .baseUri(baseURL)
@@ -45,7 +60,7 @@ public class APITest {
                 .response();
     System.out.println(response.asString());
     }
-    @Test
+    @Test(priority = 3)
     public void createNewUser(){
         response = given()
                 .baseUri(baseURL)
@@ -58,7 +73,7 @@ public class APITest {
         System.out.println(response.asString());
     }
 
-    @Test
+    @Test(priority = 1)
     public void userLogIn(){
         response = given()
                 .auth()
@@ -74,7 +89,7 @@ public class APITest {
         Assert.assertTrue(message.contains("logged in user session:"),"Message does not contain the expected value: " + message);
     }
 
-    @Test
+    @Test(priority = 4)
     public void updateUser() {
         //update the user that was created in the createNewUser() method in APITest.java
         response = given()
@@ -82,23 +97,79 @@ public class APITest {
                 .contentType("application/json")
                 .body(jsonUpdateUser)
                 .when()
-                .put(urlSuffixUpdateUser);
+                .put(urlSuffixCreateUser + "/test");
 
         testUtils.checkStatusIs200(response);
         System.out.println(response.asString());
     }
 
-    @Test
-    public void getUser() {
-        //update the user that was created in the createNewUser() method in APITest.java
+    @Test(priority = 5)
+    public void getUser() throws IOException {
+
+        String usernamePath = new String(Files.readAllBytes(Paths.get("src/test/resources/updateUser.json")), StandardCharsets.UTF_8);
+        JSONObject jsonObject = new JSONObject(usernamePath);
+        String username = jsonObject.getString("username");
+        System.out.println(username);
+
         response = given()
                 .baseUri(baseURL)
-                .contentType("application/json")
-                .body(jsonUpdateUser)
                 .when()
-                .get(urlSuffixUpdateUser);
+                .get(urlSuffixCreateUser + "/" + username);
 
         testUtils.checkStatusIs200(response);
         System.out.println(response.asString());
+    }
+
+    @Test(priority = 6)
+    public void deleteUser() throws IOException {
+        //delete the updated User
+        String usernamePath = new String(Files.readAllBytes(Paths.get("src/test/resources/updateUser.json")), StandardCharsets.UTF_8);
+        JSONObject jsonObject = new JSONObject(usernamePath);
+        String username = jsonObject.getString("username");
+        System.out.println(username);
+
+        response = given()
+                .baseUri(baseURL)
+                .when()
+                .delete(urlSuffixCreateUser + "/" + username);
+
+        testUtils.checkStatusIs200(response);
+    }
+
+    @Test(priority = 7)
+    public void getUserAfterDelete() throws IOException {
+
+        String usernamePath = new String(Files.readAllBytes(Paths.get("src/test/resources/updateUser.json")), StandardCharsets.UTF_8);
+        JSONObject jsonObject = new JSONObject(usernamePath);
+        String username = jsonObject.getString("username");
+        System.out.println(username);
+
+        response = given()
+                .baseUri(baseURL)
+                .when()
+                .get(urlSuffixCreateUser + "/" + username);
+
+        testUtils.checkStatusIs404(response);
+        System.out.println(response.asString());
+    }
+    @Test(priority = 8)
+    public void petSchemaValidation() {
+        //validated the json schema for adding a new pet
+
+        JsonSchemaFactory jsonSchemaFactory = JsonSchemaFactory.newBuilder().setValidationConfiguration(ValidationConfiguration.newBuilder().setDefaultVersion(DRAFTV4).freeze()).freeze();
+
+        response = given()
+                .baseUri(baseURL)
+                .contentType("application/json")
+                .body(jsonAddPet)
+                .post(urlSuffixAddPet)
+                .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("responseAddPet.json").using(jsonSchemaFactory))
+                .extract()
+                .response();
+    System.out.println(response.asString());
+
+
     }
 }
--- API/src/test/java/utils/TestUtils.java
+++ API/src/test/java/utils/TestUtils.java
@@ -8,4 +8,8 @@ public class TestUtils {
     public void checkStatusIs200(Response response) {
         Assert.assertEquals(response.getStatusCode(),200,"Status code is not the expected one!");
     }
+
+    public void checkStatusIs404(Response response) {
+        Assert.assertEquals(response.getStatusCode(),404,"Status code is not the expected one!");
+    }
 }
